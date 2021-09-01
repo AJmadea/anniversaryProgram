019e4a70a8d3e973c4ba9592bb25d2e59c92954c
Andrew Madea <79178009+AJmadea@users.noreply.github.com>
2021-08-31 22:08:17 -0400

    Implementing HashMap-based probability
    This class now considers the number of times each phrase has been picked from a Serialized hashmap.
    
    This class now has the Map<Integer, Integer> map as a frequency table that uses the phrase id and the number of appearances for the phrase.  The method setSayingMapDependent(Random rand) calculates the probability for each phrase.  The probability of appearance is inversely proportional to how many times its appeared.  updateMap() updates the map while initMap() instantiates the map for current/new save files.

--- RandomPhraseGenerator.java
+++ RandomPhraseGenerator.java
@@ -1,4 +1,4 @@
-package cutie;
+package source;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -14,7 +14,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
 
@@ -30,7 +32,10 @@ public final class RandomPhraseGenerator implements Serializable {
 	private transient List<String> sayings;
 	private String currentString;
 	private LocalDate lastUpdated;
+	private transient Map<String, Integer> db_map;
+	private HashMap<Integer, Integer> map;
 	private static transient DebugLog dl;
+	public transient boolean db_fail_flag = true;
 	
 	static {
 		dir = System.getProperty("user.dir");
@@ -41,14 +46,95 @@ public final class RandomPhraseGenerator implements Serializable {
 		sayings = new ArrayList<>();
 		lastUpdated = ld;
 		loadSayings();
+		initMap();
 		setSayingDateIndependent(rand);
+		
 		dl.logLn("Creating rpg via constructor");
 	}
 	
 	private void setSayingDateIndependent(Random rand) {
-		currentString = sayings.get(rand.nextInt(sayings.size()-1));
+		setSayingMapDependent(rand);
+		updateMap(currentString);
 	}
 
+	/*
+	 * Gets a saying from the database where the chance is inversely proportional to how many times the phrase
+	 * appeared.
+	 */
+	private void setSayingMapDependent(Random rand) {
+		RandomCollection<String> rc = new RandomCollection<>(rand);
+		
+		// Reverse the db_map
+		Map<Integer, String> rev_db_map = db_map.entrySet().stream()
+				.collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+		
+		/*
+		 * Creating the weights map.  the weight is equal to 1/(V+1) / Sum
+		 */
+		Map<Integer, Double> weights = new HashMap<>();
+		for (Integer i : map.keySet()) {
+			weights.put(i,  1.0/(map.get(i)+1));
+		}
+
+		// Normalizing the weights such that the sum is 1.0
+		for (Integer i : weights.keySet()) {
+			double w = weights.get(i);
+			weights.put(i, w);
+		}
+		
+		dl.logLn("Weights map:");
+		dl.logMap(weights, t -> true);
+		
+		// Adding the weights and strings to the RandomCollection object.
+		for (Integer i : rev_db_map.keySet()) 
+			rc.add(weights.get(i), rev_db_map.get(i));
+		
+		currentString = rc.next();
+	}
+	
+	private void getRandomSayingFromList(Random rand) {
+		currentString = sayings.get(rand.nextInt(sayings.size()-1));
+	}
+	
+	private void updateMap(String str) {
+		int id = db_map.get(str);
+		dl.logLn("map == null " + (map == null));
+		//System.out.println(map);
+		
+		if (map == null) initMap();
+		
+		if (map.containsKey(id)) {
+			int temp = map.get(id);
+			map.replace(id, temp+1);
+		} else { 
+			map.put(id, 1);
+		}
+		dl.logLn("Freq Map");
+		dl.logMap(map, t -> t > 0);
+	}
+	
+	private void updateMapID() {
+		for(String str : db_map.keySet()) {
+			int id = db_map.get(str);
+			if(!map.containsKey(id)) 
+				map.put(id, 0);
+		}
+	}
+	
+	private void initMap() {
+		if (map == null) {
+			map = new HashMap<Integer, Integer>();
+			if (db_map != null) 
+				db_map.forEach((k,v) -> map.put(v, 0));
+			
+			dl.logLn("Initialized a new map.");
+			System.out.println("Init new map");
+		} else {
+			dl.logLn("Map was loaded from save file.");
+			System.out.println("Map loaded from file.");
+		}
+	}
+	
 	protected static RandomPhraseGenerator init(Random r, DebugLog dl) {
 		RandomPhraseGenerator rpg = null;
 		if(Files.notExists(Paths.get(dir,"saves"))) {
@@ -78,10 +164,12 @@ public final class RandomPhraseGenerator implements Serializable {
 				if(o instanceof RandomPhraseGenerator) {
 						rpg = (RandomPhraseGenerator)o;
 						RandomPhraseGenerator.dl = dl;
+						
 						rpg.loadSayings();
+						rpg.initMap();
 						rpg.setSaying(r);	
 						rpg.updateDate(LocalDate.now());
-						RandomPhraseGenerator.dl.logLn("Creating rpg via parsing the save file :)");
+						RandomPhraseGenerator.dl.logLn("Creating rpg via parsing the save file");
 						RandomPhraseGenerator.dl.logLn(rpg.toString());
 				}
 			} catch(ClassNotFoundException e) {
@@ -99,7 +187,7 @@ public final class RandomPhraseGenerator implements Serializable {
 	
 	protected static void exit(RandomPhraseGenerator rpg) {
 		// Exit will always run after init
-		if(rpg == null) throw new IllegalArgumentException("RPG is null.  I shouldn't write a null object to the file.");
+		if(rpg == null) throw new IllegalArgumentException("RPG is null.  Cannot write a null object to the file.");
 		
 		Path p = Paths.get(dir,saveFile);
 		try (var oos = new ObjectOutputStream(
@@ -120,13 +208,16 @@ public final class RandomPhraseGenerator implements Serializable {
 	}
 	
 	private void loadSayings() {
-		PhraseDB pdb = new PhraseDB();
 		dl.logLn("Attempting to connect to the DataBase...");
 		
 		try {
-			sayings = pdb.get_phrases(dl);
+			PhraseDB pdb = new PhraseDB(dl);
+			this.db_map = pdb.getMap();
+			sayings = pdb.getPhrases();
+			
 		} catch(Exception e) {
 			dl.logLn("Something went wrong when trying to connect.  Will try to parse from the normal file");
+			db_fail_flag = false;
 			Path p = Paths.get(dir,sayingsPath);
 			try {
 				sayings = Files.lines(p).collect(Collectors.toList());
@@ -157,8 +248,15 @@ public final class RandomPhraseGenerator implements Serializable {
 			return;
 		}
 		if(shouldUpdateSaying()) {
-			currentString = sayings.get(rand.nextInt(sayings.size()));
-			dl.logLn("Updated the phrase in RPG");
+			if(db_fail_flag) {
+				updateMapID();
+				setSayingMapDependent(rand);
+				updateMap(currentString);
+			} else {
+				getRandomSayingFromList(rand);
+			}
+			
+			dl.logLn("Updated the phrase");
 		} else {
 			dl.logLn("Not enough time has passed to update the phrase");
 		}
